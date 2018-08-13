0da5ccb156ed96d5c900ab64c2a84498a6ac1057
William CLOT <contact@williamclot.com>
2018-08-13 14:39:42 +0100

    Correcting inverse head error on king kong pose

--- src/components/Selector.js
+++ src/components/Selector.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import Editor from "./Editor";
 import axios from "axios";
+import PerfectScrollbar from 'react-perfect-scrollbar'
 
+import 'react-perfect-scrollbar/dist/css/styles.css';
 import "../css/selector.css";
 
 import defaultMeshes from '../library/defaultMeshes.json';
