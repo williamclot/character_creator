0f79c6e5f9ab8271e392602d70f698e66299e2ea
William CLOT <contact@williamclot.com>
2018-08-13 14:47:41 +0100

    Cleaning imports

--- src/components/Selector.js
+++ src/components/Selector.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import Editor from "./Editor";
 import axios from "axios";
-import PerfectScrollbar from 'react-perfect-scrollbar'
 
 import 'react-perfect-scrollbar/dist/css/styles.css';
 import "../css/selector.css";
