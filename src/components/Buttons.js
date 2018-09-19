c189f6e67f164184b8d5c4b18fb10f4a31e29739
William CLOT <contact@williamclot.com>
2018-09-19 17:09:17 +0100

    Changing css of upload form

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -11,7 +11,7 @@ class Buttons extends Component {
   constructor(props){
     super(props);
     this.state = {
-      formVisible: true,
+      formVisible: false,
       accesstoken: ''
     }
   }
