60ed8df99e9808904f31406d5ca205ea18f4d852
del <adrian.delgadovlaic@gmail.com>
2018-10-29 16:45:28 +0000

    remove publicUrl.sh script

--- publicUrl.sh
+++
@@ -1,8 +0,0 @@
-#Paths to public_html folder
-HTLM='../mmf/public_html/character-creator'
-
-#Inserting first line publicURL
-sed -i "1s|^.*|var publicUrl = '$1';\n|g" build/js/skeleton.js
-#Copying new files to the mmf folder
-rm -r ../mmf/public_html/character-creator
-cp -r build ../mmf/public_html/character-creator
