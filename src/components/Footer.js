340b010b9cad74a4a7dceee61448507a8f08307c
del <adrian.delgadovlaic@gmail.com>
2018-11-01 16:20:06 +0000

    Update Footer to use MMF endpoint env variable

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,8 @@ import Popup from './Popup';
 
 import "../css/footer.css";
 
+import { LIVE_MMF_ENDPOINT } from '../config';
+
 class Footer extends Component {
 
   constructor(props) {
@@ -28,13 +30,13 @@ class Footer extends Component {
         <div className="licence abs bottom">
           <a onClick={ () => {
             const content = <span className="a-licence">Licensing associated with files download from Character Creator (beta):
-              <a href="https://www.myminifactory.com/pages/object-licensing">Credit Designer</a>,
-              <a href="https://www.myminifactory.com/pages/object-licensing">Remixing Allowed</a>,
-              <a href="https://www.myminifactory.com/pages/object-licensing">Commercial Use Not Allowed</a></span>
+              <a href={`${LIVE_MMF_ENDPOINT}/pages/object-licensing`}>Credit Designer</a>,
+              <a href={`${LIVE_MMF_ENDPOINT}/pages/object-licensing`}>Remixing Allowed</a>,
+              <a href={`${LIVE_MMF_ENDPOINT}/pages/object-licensing`}>Commercial Use Not Allowed</a></span>
             this.setState({popup: true, message: content});
           }}>Licence Information</a> | 
           <a onClick={() => {
-            const content = <span>If you are a Designer or a Brand who would like to create their own custom Creator tool, or if you have any other questions, please get in touch via our <a href="https://www.myminifactory.com/contact_us/">contact form.</a></span>
+            const content = <span>If you are a Designer or a Brand who would like to create their own custom Creator tool, or if you have any other questions, please get in touch via our <a href={`${LIVE_MMF_ENDPOINT}/contact_us/`}>contact form.</a></span>
             this.setState({popup: true, message: content});
           }}> Want to create you're own customizer or contact us?</a>
         </div>
