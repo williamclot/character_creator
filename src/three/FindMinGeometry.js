95867d1f258342fc5bcee927f97b01f7b85a0e21
del <adrian.delgadovlaic@gmail.com>
2018-11-12 15:20:17 +0000

    Refactor main loop to not use geometry
    In order to find the lowest point inside a
    Mesh, it is necessary to traverse the geometry and
    perform some calculations.
    
    The geometry of the Mesh is of type
    BufferGeometry and it is converted to normal
    Geometry in order to perform the calculations.
    
    The problem is that during the conversion from
    BufferGeometry to Geometry, data about
    skinIndices and skinWeights is lost (either a bug or
    it was inteded to behave like this).
    
    The current workaround is to modify the three.js
    library such that it preserves the required data
    during conversion.
    
    This commit eliminates the need to convert the
    geometry from BufferGeometry to normal Geometry
    by getting all the data straight from the attributes
    of the buffered geometry. It then performs the exact
    same calculations in order to find out the lowest point in the geometry.
    
    This closes #15, closes #16

--- src/three/FindMinGeometry.js
+++ src/three/FindMinGeometry.js
@@ -27,25 +27,26 @@ THREE.FindMinGeometry.prototype = {
 			scene.traverse( function ( mesh ) {
 				
 				if ( mesh instanceof THREE.Mesh ) {
-					
-					var matrixWorld = mesh.matrixWorld;
 
-					var bufferGeometry = mesh.geometry;
-					var geometry = new THREE.Geometry().fromBufferGeometry( bufferGeometry );
+					const {
+						matrixWorld,
+						geometry: bufferGeometry,
+						skeleton
+					} = mesh;
+					
+					// var geometry = new THREE.Geometry().fromBufferGeometry( bufferGeometry );
 					
 					const bufferIndices = bufferGeometry.getIndex();
-					// const indices = bufferIndices.array;
 					const bufferSkinIndices = bufferGeometry.getAttribute('skinIndex');
 					const bufferSkinWeights = bufferGeometry.getAttribute('skinWeight');
 					const bufferPositions = bufferGeometry.getAttribute('position');
-					const positions = bufferPositions.array;
 
 					function computeFaceNormal(a, b, c) {
 						const cb = new THREE.Vector3(), ab = new THREE.Vector3();
 					
-						const vA = new THREE.Vector3().fromArray(positions, a * bufferPositions.itemSize);
-						const vB = new THREE.Vector3().fromArray(positions, b * bufferPositions.itemSize);
-						const vC = new THREE.Vector3().fromArray(positions, c * bufferPositions.itemSize);
+						const vA = new THREE.Vector3().fromBufferAttribute(bufferPositions, a);
+						const vB = new THREE.Vector3().fromBufferAttribute(bufferPositions, b);
+						const vC = new THREE.Vector3().fromBufferAttribute(bufferPositions, c);
 					
 						cb.subVectors( vC, vB );
 						ab.subVectors( vA, vB );
@@ -55,92 +56,63 @@ THREE.FindMinGeometry.prototype = {
 
 						return cb;
 					}
-					
-					// console.log(geometry)
 
-					if ( geometry instanceof THREE.Geometry ) {
+					normalMatrixWorld.getNormalMatrix( matrixWorld );
 
-						var vertices = geometry.vertices;
-						var faces = geometry.faces;
+					for ( let i = 0, len = bufferIndices.count; i < len; i += 3 ) {
+						const a = bufferIndices.getX(i);
+						const b = bufferIndices.getY(i);
+						const c = bufferIndices.getZ(i);
 
-						normalMatrixWorld.getNormalMatrix( matrixWorld );
+						const faceNormal = computeFaceNormal(a, b, c);
 
-						// for ( let i = 0, len = bufferIndices.count; i < len; i += 3 ) {
-						// 	const a = bufferIndices.getX(i);
-						// 	const b = bufferIndices.getY(i);
-						// 	const c = bufferIndices.getZ(i);
+						// this may be redundant
+						vector.copy(faceNormal).applyMatrix3(normalMatrixWorld).normalize();
 
-						// 	const faceNormal = computeFaceNormal(a, b, c);
+						[a,b,c].forEach(vertexIndex => {
 
-						// 	vector.copy(faceNormal).applyMatrix3(normalMatrixWorld).normalize();
+							vector.fromBufferAttribute(bufferPositions, vertexIndex);
 
-						// 	[a,b,c].forEach(vertexIndex => {
-						// 		// vector.copy( vertices[ vertexIndex ] );
-						// 		vector.fromArray(positions, vertexIndex)
+							let boneIndices = [];
+							boneIndices[0] = bufferSkinIndices.getX(vertexIndex);
+							boneIndices[1] = bufferSkinIndices.getY(vertexIndex);
+							boneIndices[2] = bufferSkinIndices.getZ(vertexIndex);
+							boneIndices[3] = bufferSkinIndices.getW(vertexIndex);
+							
+							let weights = [];
+							weights[0] = bufferSkinWeights.getX(vertexIndex);
+							weights[1] = bufferSkinWeights.getY(vertexIndex);
+							weights[2] = bufferSkinWeights.getZ(vertexIndex);
+							weights[3] = bufferSkinWeights.getW(vertexIndex);
+							
+							let inverses = [];
+							inverses[0] = skeleton.boneInverses[ boneIndices[0] ];
+							inverses[1] = skeleton.boneInverses[ boneIndices[1] ];
+							inverses[2] = skeleton.boneInverses[ boneIndices[2] ];
+							inverses[3] = skeleton.boneInverses[ boneIndices[3] ];
 
-								
-						// 	});
-						// }
-
-						for ( var i = 0, l = faces.length; i < l; i ++ ) {
-							var face = faces[ i ];
-
-							vector.copy( face.normal ).applyMatrix3( normalMatrixWorld ).normalize();
-
-							var indices = [ face.a, face.b, face.c ];
-
-							for ( var j = 0; j < 3; j ++ ) {
-								var vertexIndex = indices[ j ];
-								if (geometry.skinIndices.length == 0) {
-									vector.copy( vertices[ vertexIndex ] ).applyMatrix4( matrixWorld );
-									output += '\t\t\tvertex ' + vector.x + ' ' + vector.y + ' ' + vector.z + '\n';
-								} else {
-									vector.copy( vertices[ vertexIndex ] ); //.applyMatrix4( matrixWorld );
-									
-									// see https://github.com/mrdoob/three.js/issues/3187
-									let boneIndices = [];
-									boneIndices[0] = geometry.skinIndices[vertexIndex].x;
-									boneIndices[1] = geometry.skinIndices[vertexIndex].y;
-									boneIndices[2] = geometry.skinIndices[vertexIndex].z;
-									boneIndices[3] = geometry.skinIndices[vertexIndex].w;
-									
-									let weights = [];
-									weights[0] = geometry.skinWeights[vertexIndex].x;
-									weights[1] = geometry.skinWeights[vertexIndex].y;
-									weights[2] = geometry.skinWeights[vertexIndex].z;
-									weights[3] = geometry.skinWeights[vertexIndex].w;
-									
-									let inverses = [];
-									inverses[0] = mesh.skeleton.boneInverses[ boneIndices[0] ];
-									inverses[1] = mesh.skeleton.boneInverses[ boneIndices[1] ];
-									inverses[2] = mesh.skeleton.boneInverses[ boneIndices[2] ];
-									inverses[3] = mesh.skeleton.boneInverses[ boneIndices[3] ];
-
-									let skinMatrices = [];
-									skinMatrices[0] = mesh.skeleton.bones[ boneIndices[0] ].matrixWorld;
-									skinMatrices[1] = mesh.skeleton.bones[ boneIndices[1] ].matrixWorld;
-									skinMatrices[2] = mesh.skeleton.bones[ boneIndices[2] ].matrixWorld;
-									skinMatrices[3] = mesh.skeleton.bones[ boneIndices[3] ].matrixWorld;
-									
-									var finalVector = new THREE.Vector4();
-									for(var k = 0; k<4; k++) {
-										var tempVector = new THREE.Vector4(vector.x, vector.y, vector.z);
-										tempVector.multiplyScalar(weights[k]);
-										//the inverse takes the vector into local bone space
-										tempVector.applyMatrix4(inverses[k])
-										//which is then transformed to the appropriate world space
-										.applyMatrix4(skinMatrices[k]);
-										finalVector.add(tempVector);
-									}
-									if (output > finalVector.y){
-										output = finalVector.y;
-									}
-								}
+							let skinMatrices = [];
+							skinMatrices[0] = skeleton.bones[ boneIndices[0] ].matrixWorld;
+							skinMatrices[1] = skeleton.bones[ boneIndices[1] ].matrixWorld;
+							skinMatrices[2] = skeleton.bones[ boneIndices[2] ].matrixWorld;
+							skinMatrices[3] = skeleton.bones[ boneIndices[3] ].matrixWorld;
+							
+							var finalVector = new THREE.Vector4();
+							for(var k = 0; k<4; k++) {
+								var tempVector = new THREE.Vector4(vector.x, vector.y, vector.z);
+								tempVector.multiplyScalar(weights[k]);
+								//the inverse takes the vector into local bone space
+								tempVector.applyMatrix4(inverses[k])
+								//which is then transformed to the appropriate world space
+								.applyMatrix4(skinMatrices[k]);
+								finalVector.add(tempVector);
 							}
-						}
+							if (output > finalVector.y){
+								output = finalVector.y;
+							}
+						});
 					}
 				}
-
 			} );
 
 			return output;
