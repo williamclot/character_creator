f575bfc3762d14c2b1ccd291013acb91d1feb6be
del <adrian.delgadovlaic@gmail.com>
2018-11-13 17:36:10 +0000

    Add index.js file to three/

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import ThreeContainer from './three/ThreeContainer';
+import ThreeContainer from './three';
 import registerServiceWorker from './registerServiceWorker';
 
 // needed to polyfill THREE global object with the class FindMinGeometry
