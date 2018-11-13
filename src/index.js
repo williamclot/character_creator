aa7bb6e686b9a365757bfc526d3960d27e61a212
del <adrian.delgadovlaic@gmail.com>
2018-11-13 17:41:31 +0000

    Remove findMinGeometry import from index.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ import App from './App';
 import ThreeContainer from './three';
 import registerServiceWorker from './registerServiceWorker';
 
-// needed to polyfill THREE global object with the class FindMinGeometry
-import './three/FindMinGeometry';
-
 ReactDOM.render(
     <React.Fragment>
         <ThreeContainer />
