da5c94fef65e66bbae9b6d7aef4bd23c5b8c1923
del <adrian.delgadovlaic@gmail.com>
2019-01-21 13:29:21 +0000

    configure store to use browser extension

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { createStore } from 'redux'
 import { Provider } from 'react-redux'
-import rootReducer from './reducers'
+import store from './store'
 
 import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
-
-
-const store = createStore( rootReducer )
 
 ReactDOM.render(
     <Provider store = { store }>
