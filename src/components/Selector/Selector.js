b722d318efd8b5ca0848160e70d64c47e8aa1928
del <adrian.delgadovlaic@gmail.com>
2019-01-23 18:04:03 +0000

    add selector component

---
+++ src/components/Selector/Selector.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+import './Selector.css'
+
+class Selector extends Component {
+    constructor( props ) {
+        super( props )
+    }
+
+    render() {
+        return (
+            <div className = "selector">
+                <h1>SELECT</h1>
+            </div>
+        )
+    }
+}
+
+export default Selector
