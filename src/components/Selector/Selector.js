3336f0803f620460f110d96c9a99394474608b8e
del <adrian.delgadovlaic@gmail.com>
2019-01-24 12:28:06 +0000

    add sample data in selector and copy css from other repo

--- src/components/Selector/Selector.js
+++ src/components/Selector/Selector.js
@@ -5,12 +5,38 @@ import './Selector.css'
 class Selector extends Component {
     constructor( props ) {
         super( props )
+
+        // console.log(props.objects)
+    }
+
+    handleClick = index => {
+        console.log( 'clicked', index )
     }
 
     render() {
+        const { objects } = this.props
+
+        const elementDiv = objects.map( ( objUrl, index ) => (
+			<div
+				className = "selector-item"
+				key = {objUrl}
+				onClick = { () => this.handleClick( objUrl ) }
+			>
+				<div className = "img">
+					<img
+						src = { null }
+						alt = "img"
+					/>
+				</div>
+				<div className = "unselectable item-name">
+					{ objUrl.slice( 29, -4 ) }
+				</div>
+			</div>
+		))
+
         return (
             <div className = "selector">
-                <h1>SELECT</h1>
+                { elementDiv }
             </div>
         )
     }
