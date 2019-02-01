4f8723a283935775ef54fb52b3dd9e2b6d5a225e
del <adrian.delgadovlaic@gmail.com>
2019-02-01 18:47:54 +0000

    add custom config for build script

---
+++ config/webpack.config.prod.js
@@ -0,0 +1,45 @@
+const path = require('path')
+const { appPath } = require('./paths')
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'index.js',
+        path: path.resolve(appPath, 'dist'),
+
+        library: 'ReactCustomizer',
+        libraryTarget: 'umd'
+    },
+
+    mode: 'production',
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules\//,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'index.css'
+        })
+    ],
+
+    // externals: {
+    //     react: 'react',
+    //     'react-dom': 'react-dom'
+    // }
+}
