0f284b893a00ce669d029f1a8f8d6b33df406e25
del <adrian.delgadovlaic@gmail.com>
2019-02-04 12:39:34 +0000

    add react to externals in webpack config

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -38,8 +38,8 @@ module.exports = {
         })
     ],
 
-    // externals: {
-    //     react: 'react',
-    //     'react-dom': 'react-dom'
-    // }
+    externals: {
+        react: 'react',
+        // 'react-dom': 'react-dom'
+    }
 }
