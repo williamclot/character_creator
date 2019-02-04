f06259c097dd51220361585ea7f2131e517b0d04
del <adrian.delgadovlaic@gmail.com>
2019-02-04 17:38:29 +0000

    set index.dev.js as entry point in dev mode

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -138,7 +138,7 @@ module.exports = function(webpackEnv) {
       isEnvDevelopment &&
         require.resolve('react-dev-utils/webpackHotDevClient'),
       // Finally, this is your app's code:
-      paths.appIndexJs,
+      paths.appDevIndexJs,
       // We include the app code last so that if there is a runtime error during
       // initialization, it doesn't blow up the WebpackDevServer client, and
       // changing JS code would still trigger a refresh.
--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,1 +1,12 @@
-export { default } from './App'
+import React from 'react'
+import { Provider } from 'react-redux'
+
+import ConnectedApp from './App'
+import store from '../../store'
+
+
+export default props => (
+    <Provider store = { store }>
+        <ConnectedApp {...props} />
+    </Provider>
+)
