f9e50915d2cc625703e5c9ebad293084c6265676
Adrian Delgado <adrian.delgadovlaic@gmail.com>
2019-02-11 18:52:35 +0000

    add default selection

--- src/reducers/selectedCategoryPath.js
+++ src/reducers/selectedCategoryPath.js
@@ -1,9 +1,15 @@
-const selectedCategoryPath = ( state = {}, action ) => {
+const initialState = {
+    groupIndex: 0,
+    categoryIndex: 0
+}
+
+const selectedCategoryPath = ( state = initialState, action ) => {
     switch( action.type ) {
 
         case 'SET_CURRENT_GROUP': {
             return {
-                groupIndex: action.index
+                groupIndex: action.index,
+                categoryIndex: 0
             }
         }
         
