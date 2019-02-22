76f5a6c975a1c23918b8388a6f11f76ab264da2f
del <adrian@myminifactory.com>
2019-02-22 02:28:13 +0000

    [AdjustTransforms] make class component

--- src/components/UploadWizard/AdjustTransforms.js
+++ src/components/UploadWizard/AdjustTransforms.js
@@ -1,19 +1,23 @@
-import React from 'react'
+import React, { Component } from 'react'
 import classNames from 'classnames'
 
-const AdjustTransforms = ({
-    visible,
-    nextStep, previousStep
-}) => {
-    return (
-        <div
-            className = {classNames('wizard-step', 'adjust-transforms', { visible })}
-        >
-            ADJUST POSITION, ROTATION AND SCALE <br/>
-            <button onClick = { previousStep }>previous</button>
-            <button onClick = { nextStep }>next</button>
-        </div>
-    )
+class AdjustTransforms extends Component {
+    render() {
+        const {
+            visible,
+            nextStep, previousStep
+        } = this.props
+
+        return (
+            <div
+                className = {classNames('wizard-step', 'adjust-transforms', { visible })}
+            >
+                ADJUST POSITION, ROTATION AND SCALE <br/>
+                <button onClick = { previousStep }>previous</button>
+                <button onClick = { nextStep }>next</button>
+            </div>
+        )
+    }
 }
 
 export default AdjustTransforms
