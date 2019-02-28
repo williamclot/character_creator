095d8f6a6d81888064b1267a01d840bc5f92d6da
del <adrian@myminifactory.com>
2019-02-28 10:51:16 +0000

    delete UploadWizard.js and move code to index.js

--- src/components/UploadWizard/index.js
+++ src/components/UploadWizard/index.js
@@ -1,1 +1,156 @@
-export { default } from './UploadWizard'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import classNames from 'classnames'
+
+import UploadConfirm from './UploadConfirm'
+import AdjustTransforms from './AdjustTransforms'
+import PlaceAttachpoint from './PlaceAttachpoint'
+
+import './UploadWizard.css'
+import { steps, previousStep, nextStep } from '../../actions/steps'
+
+import { stlLoader } from '../../util/loaders'
+
+class UploadWizard extends Component {
+    constructor( props ) {
+        super( props )
+
+        this.state = {
+
+            // handled by UploadConfirm
+            name: '',
+            uploadedObjectGeometry: null,
+
+            // handled by AdjustTransforms
+            position: null,
+            rotation: null,
+            scale: null,
+
+            // handled by PlaceAttachpoint
+            attachPoints: null
+        }
+    }
+
+    async componentDidMount() {
+        const { data } = this.props
+
+        if ( !data ) { return }
+
+        const {
+            name, extension,
+            objectURL
+        } = data
+
+        try {
+
+            const geometry = await stlLoader.load( objectURL )
+
+            if ( !geometry.boundingBox ) {
+                geometry.computeBoundingBox()
+            }
+
+            this.setState({
+                name,
+                uploadedObjectGeometry: geometry
+            })
+
+        } catch ( err ) {
+
+            console.error( err )
+            return
+
+        } finally {
+
+            // cleanup to prevent memory leaks
+            URL.revokeObjectURL( objectURL )
+
+        }
+    }
+
+    onNameChange = e => {
+        this.setState({
+            name: e.target.value
+        })
+    }
+
+    setPosition = pivotPointVector => {
+        console.log( pivotPointVector )
+
+        const { x, y, z } = pivotPointVector.negate() // invert pos
+
+        const computedPos = { x, y, z }
+
+        this.setState({
+            position: computedPos
+        })
+    }
+
+    render() {
+        const {
+            currentCategory, data,
+            onWizardCanceled, onWizardCompleted,
+            step, nextStep, previousStep
+        } = this.props
+        const { defaultRotation } = data
+
+
+        const {
+            name, uploadedObjectGeometry
+        } = this.state
+    
+    
+        return <>
+            <div className = "wizard-background" />
+            <div className = "wizard-container">
+    
+                <UploadConfirm
+                    visible = { step === steps.UPLOAD_CONFIRM }
+    
+                    currentCategory = { currentCategory }
+                    name = { name }
+                    onNameChange = { this.onNameChange }
+                    uploadedObjectGeometry = { uploadedObjectGeometry }
+    
+                    onCancel = { onWizardCanceled }
+                    onNext = { nextStep }
+                />
+    
+                <PlaceAttachpoint
+                    visible = { step === steps.PLACE_ATTACHPOINT }
+                    
+                    currentCategory = { currentCategory }
+                    defaultRotation = { defaultRotation }
+                    uploadedObjectGeometry = { uploadedObjectGeometry }
+                    onClick = { this.setPosition }
+
+                    previousStep = { previousStep }
+                    nextStep = { nextStep }
+                />
+    
+                <AdjustTransforms
+                    visible = { step === steps.ADJUST }
+
+                    uploadedObjectGeometry = { uploadedObjectGeometry }
+
+                    previousStep = { previousStep }
+                    nextStep = { nextStep }
+                />
+    
+            </div>
+        </>
+    }
+}
+
+const mapStateToProps = state => ({
+    step: state.step
+})
+
+const mapDispatchToProps = {
+    nextStep,
+    previousStep
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)( UploadWizard )
