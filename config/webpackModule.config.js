6e5cc7a0fcc6713e959521b9e0a008ea4394e1ab
del <adrian@myminifactory.com>
2019-03-29 19:01:55 +0000

    increase webpack asset size limit to 15kb

--- config/webpackModule.config.js
+++ config/webpackModule.config.js
@@ -115,7 +115,7 @@ module.exports = {
                         test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
                         loader: require.resolve('url-loader'),
                         options: {
-                            limit: 10000,
+                            limit: 15000,
                             name: 'static/media/[name].[hash:8].[ext]',
                         },
                     },
