383007d7b447a1825e9a8cc28afaaf4c55e06a8a
Adrian Delgado <adrian.delgadovlaic@gmail.com>
2019-04-09 14:13:20 +0100

    refactor input to use DraggableInput and NormalInput

--- src/components/MyInput/index.js
+++ src/components/MyInput/index.js
@@ -1,19 +1,20 @@
-import React, { Component, createRef } from 'react'
+import React, { Component } from 'react'
 import classNames from 'classnames'
 
-import Input from './Input'
+import NormalInput from './NormalInput'
+import DraggableInput from './DraggableInput';
 
 import styles from './index.module.css'
 
-const LEFT_MOUSE_BUTTON = 0
-
+/**
+ * @type { Props }
+ */
 const defaultProps = {
     step: 1,
     min: -100,
     max:  100,
     precision: 3,
 
-    /** @type { Formatter } */
     formatter: {
         format: number => number,
         parse: text => text
@@ -27,89 +28,27 @@ export default class NumberInput extends Component {
         this.state = {
             isSelected: false
         }
-
-        this.prevPointer = null
-        this.distance = null
-        this.startValue = null
-
     }
 
-    componentWillUnmount() {
-        // in case component is unmounted during drag
-        document.removeEventListener( 'mouseup', this.onMouseUp )
-        document.removeEventListener( 'mousemove', this.onMouseMove )
+    handleClick = () => {
+        this.setState({
+            isSelected: true
+        })
     }
 
-    onMouseDown = event => {
-        if ( event.button !== LEFT_MOUSE_BUTTON ) return
-
-        event.preventDefault()
-
-        this.prevPointer = {
-            x: event.clientX,
-            y: event.clientY,
-        }
-        this.distance = 0
-        this.startValue = this.props.value
-
-        document.addEventListener( 'mouseup', this.onMouseUp )
-        document.addEventListener( 'mousemove', this.onMouseMove )
+    handleBlur = () => {
+        this.setState({
+            isSelected: false
+        })
     }
-
-    onMouseMove = event => {
-        const { step, min, max } = this.props
-
-        const {
-            clientX, clientY,
-            shiftKey: isShiftPressed
-        } = event
-
-        const {
-            x: prevX,
-            y: prevY
-        } = this.prevPointer
-
-        const deltaX = clientX - prevX
-        const deltaY = clientY - prevY
-
-        this.distance += ( deltaX - deltaY )
-
-        const dividingFactor = isShiftPressed ? 5 : 50
-        const normalizedDistance = ( this.distance / dividingFactor )
-
-        const value = this.startValue + normalizedDistance * step
-        
-        const clampedValue = Math.min( max, Math.max( min, value ) )
-
-        this.props.onChange( clampedValue )
-
-        this.prevPointer = {
-            x: clientX,
-            y: clientY
-        }
-    }
-
-    onMouseUp = () => {
-        const totalDistanceMoved = Math.abs( this.distance )
-
-        if ( totalDistanceMoved < 2 ) {
-            console.log('normal click')
-        }
-
-        document.removeEventListener( 'mouseup', this.onMouseUp )
-        document.removeEventListener( 'mousemove', this.onMouseMove )
-    }
-
 
     render() {
         const {
-            axis, value,
-            precision, min, max, step,
-            formatter
+            axis,
+            value, onChange,
+            precision, min, max, step, formatter
         } = this.props
 
-        const formattedValue = formatter.format( value )
-        const fixedValue = Number(formattedValue).toFixed( precision )
 
         const hasAxis = Boolean( axis )
         const inputClassName = classNames(
@@ -126,12 +65,32 @@ export default class NumberInput extends Component {
                     </div>
                 )}
 
-                <span
-                    className = { inputClassName }
-                    onMouseDown = { this.onMouseDown }
-                >
-                    {fixedValue}
-                </span>
+                {this.state.isSelected ?
+                    <NormalInput
+                        className = { inputClassName }
+                        onBlur = { this.handleBlur }
+        
+                        value = { value }
+                        onChange = { onChange }
+                        formatter = { formatter }
+                        min = { min }
+                        max = { max }
+                        precision = { precision }
+                    />
+                    :
+                    <DraggableInput
+                        className = { inputClassName }
+                        onClick = { this.handleClick }
+
+                        value = { value }
+                        onChange = { onChange }
+                        formatter = { formatter }
+                        min = { min }
+                        max = { max }
+                        step = { step }
+                        precision = { precision }
+                    />
+                }
 
             </div>
         )
@@ -140,23 +99,32 @@ export default class NumberInput extends Component {
 
 NumberInput.defaultProps = defaultProps
 
-
 /**
- * @typedef { Object } Formatter
- * @property { FormatFunction<number> } format
- * @property { ParseFunction<number> } parse
+ * @typedef { Object } Props
+ * @property { Formatter<number, number> } formatter
+ * @property { number } step
+ * @property { number } min
+ * @property { number } max
+ * @property { number } precision
  */
 
 /**
- * @template T
+ * @template A, B
+ * @typedef { Object } Formatter
+ * @property { FormatFunction<A, B> } format
+ * @property { ParseFunction<B, A> } parse
+ */
+
+/**
+ * @template T, S
  * @callback FormatFunction
  * @param { T } value
- * @returns { string }
+ * @returns { S }
  */
 
 /**
- * @template T
+ * @template T, S
  * @callback ParseFunction
- * @param { string } value
+ * @param { S } value
  * @returns { T }
  */
