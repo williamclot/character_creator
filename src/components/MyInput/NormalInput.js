14c6ab48031fb01d06b5ddd17e5fc6841ec73bc1
Adrian Delgado <adrian.delgadovlaic@gmail.com>
2019-04-09 13:58:30 +0100

    add component for normal input with formatter

---
+++ src/components/MyInput/NormalInput.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react'
+
+const ENTER_KEY = 'Enter'
+
+class NormalInput extends Component {
+    constructor( props ) {
+        super( props )
+
+        const formattedValue = props.formatter.format( props.value )
+        const computedValue = Number(formattedValue).toFixed( props.precision )
+
+        this.state = {
+            value: computedValue
+        }
+    }
+
+    submitValue = () => {
+        const { min, max } = this.props
+        const rawValue = this.state.value
+
+        const value = this.props.formatter.parse( rawValue )
+        const clampedValue = Math.min( max, Math.max( min, value ) )
+
+        if ( !isNaN( clampedValue ) ) {
+            this.props.onChange( clampedValue )
+        }
+        this.props.onBlur()
+    }
+
+    handleChange = e => {
+        this.setState({
+            value: e.target.value
+        })
+    }
+
+    handleKeyDown = e => {
+        if ( e.key === ENTER_KEY ) {
+            this.submitValue()
+        }
+    }
+
+    handleBlur = () => {
+        this.submitValue()
+    }
+
+    render() {
+        const { className } = this.props
+        const { value } = this.state
+
+        return (
+            <input
+                className = { className }
+                type = "text"
+
+                value = { value }
+                onChange = { this.handleChange }
+                
+                onKeyDown = { this.handleKeyDown }
+                onBlur = { this.handleBlur }
+            />
+        )
+    }
+}
+
+export default NormalInput
