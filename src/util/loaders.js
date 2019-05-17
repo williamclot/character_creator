c63cdadd477429a06ac0a0ddc6c066a4e0717dd1
Adrian Delgado <adrian.delgadovlaic@gmail.com>
2019-05-17 14:04:48 +0100

    refactor(loaders): use axios for fetch
    Refactor to use axios for ajax request and stl loader to parse result.
    Additionally, comment out GLTF loader as it is not used.

--- src/util/loaders.js
+++ src/util/loaders.js
@@ -1,42 +1,45 @@
 import * as THREE from 'three';
+import axios from 'axios'
 
 import STLLoaderWrapper from 'three-stl-loader';
-import GLTFLoader from 'three-gltf-loader';
+// import GLTFLoader from 'three-gltf-loader';
 const STLLoader = STLLoaderWrapper( THREE );
 
-const _gltfLoader = new GLTFLoader
+const TIMEOUT = 10000
+
+// const _gltfLoader = new GLTFLoader
 const _stlLoader = new STLLoader
 
 
-const promisified_gltfLoader = {
-    load: function( url ) {
-        return new Promise( ( resolve, reject ) => {
-            _gltfLoader.load( url, resolve, null, reject )
+// const promisified_gltfLoader = {
+//     async load( url ) {
+//         const { data } = await axios.get( url, {
+//             timeout: TIMEOUT,
+//             responseType: 'arraybuffer'
+//         })
 
-            // timeout after 10 secs
-            setTimeout(reject, 10000, new Error('Timed out'))
-        })
-    },
-    parse: function( data, path ) {
-        return new Promise( ( resolve, reject ) => _gltfLoader.parse( data, path, resolve, reject ) )
-    }
-}
+//         return this.parse( data )
+//     },
+//     async parse( data, path ) {
+//         return new Promise( ( resolve, reject ) => _gltfLoader.parse( data, path, resolve, reject ) )
+//     },
+// }
 
 const promisified_stlLoader = {
-    load: function( url ) {
-        return new Promise( ( resolve, reject ) => {
-            _stlLoader.load( url, resolve, null, reject )
-
-            // timeout after 10 secs
-            setTimeout(reject, 10000, new Error('Timed out'))
+    async load( url ) {
+        const { data } = await axios.get( url, {
+            timeout: TIMEOUT,
+            responseType: 'arraybuffer'
         })
+
+        return this.parse( data )
     },
-    parse: function( data ) {
-        return Promise.resolve( _stlLoader.parse( data ) )
-    }
+    async parse( data ) {
+        return _stlLoader.parse( data )
+    },
 }
 
 export {
-    promisified_gltfLoader as gltfLoader,
+    // promisified_gltfLoader as gltfLoader,
     promisified_stlLoader as stlLoader
 }
