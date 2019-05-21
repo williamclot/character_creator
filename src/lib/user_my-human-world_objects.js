1694aedaab095989882af8b52bed98a88e77975c
Adrian Delgado <adrian.delgadovlaic@gmail.com>
2019-05-21 13:31:21 +0100

    feat(test-models): fix test models

--- src/lib/user_my-human-world_objects.js
+++ src/lib/user_my-human-world_objects.js
@@ -2,8 +2,43 @@ const lib = {
     "byCategory": {
         "Torso": [
             {
+                "name": "Cylinder",
+                "download_url": process.env.PUBLIC_URL + "http://localhost:3000/models/cylinder.stl",
+                "extension": "stl",
+                "metadata": {
+                    "scale": 3,
+                    "attachPoints": {
+                        "Torso_Neck": {
+                            "x": -0.008884304243588645,
+                            "y": 1.9614155888557434,
+                            "z": 0.09450079013285406
+                        },
+                        "Torso_UpperArm_L": {
+                            "x": 0.1,
+                            "y": 1.9614155888557434,
+                            "z": 0
+                        },
+                        "Torso_UpperArm_R": {
+                            "x": -0.1,
+                            "y": 1.9614155888557434,
+                            "z": 0
+                        },
+                        "Torso_UpperLeg_L": {
+                            "x": 0.2907995785808455,
+                            "y": -1.9614155888557434,
+                            "z": 0.022966212705601174
+                        },
+                        "Torso_UpperLeg_R": {
+                            "x": -0.3229941152623676,
+                            "y": -1.9614155888557434,
+                            "z": -0.07123728410431474
+                        }
+                    }
+                }
+            },
+            {
                 "name": "CUBE TORSO",
-                "download_url": process.env.PUBLIC_URL + "http://localhost:3000/models/model.stl",
+                "download_url": process.env.PUBLIC_URL + "http://localhost:3000/models/cube.stl",
                 "extension": "stl",
                 "metadata": {
                     "attachPoints": {
@@ -34,21 +69,50 @@ const lib = {
                         }
                     }
                 }
-            },        
+            },
         ],
         "Head": [
             {
                 "name": "CUBE",
-                "download_url": process.env.PUBLIC_URL + "http://localhost:3000/models/model.stl",
+                "download_url": process.env.PUBLIC_URL + "http://localhost:3000/models/cube.stl",
                 "extension": "stl",
                 "metadata": {
+                    "scale": .4
                 }
             },  
         ],
         "ArmR": [
             {
+                "name": "Cylinder",
+                "download_url": process.env.PUBLIC_URL + "http://localhost:3000/models/cylinder.stl",
+                "extension": "stl",
+                "metadata": {
+                    
+                    
+                    "position": {
+                        "x": -0.004535016565342026,
+                        "y":  0.5141420841591644,
+                        "z": -0.0021842929044645354,
+                    },
+                    "rotation": {
+                        "x": 0,
+                        "y": 0,
+                        "z": 1.9198621771937625
+                    },
+                    "scale": 0.764753787276196,
+                    
+                    "attachPoints": {
+                        "ArmR_Hand_R":{
+                            "x": -0.8369515528225573,
+                            "y": -0.3120135173281886,
+                            "z": -0.0040737141462420905
+                        }
+                    }
+                }
+            },
+            {
                 "name": "CUBE",
-                "download_url": process.env.PUBLIC_URL + "http://localhost:3000/models/model.stl",
+                "download_url": process.env.PUBLIC_URL + "http://localhost:3000/models/cube.stl",
                 "extension": "stl",
                 "metadata": {
                     "attachPoints": {
@@ -59,12 +123,40 @@ const lib = {
                         },
                     }
                 }
-            },  
+            },
         ],
         "ArmL": [
             {
+                "name": "Cylinder",
+                "download_url": process.env.PUBLIC_URL + "http://localhost:3000/models/cylinder.stl",
+                "extension": "stl",
+                "metadata": {
+                    
+                    
+                    "position": {
+                        "x": -0.004535016565342026,
+                        "y":  0.5141420841591644,
+                        "z": -0.0021842929044645354,
+                    },
+                    "rotation": {
+                        "x":  0,
+                        "y":  0,
+                        "z": -1.9198621771937625
+                    },
+                    "scale": 0.764753787276196,
+                    
+                    "attachPoints": {
+                        "ArmL_Hand_L":{
+                            "x": 0.8369515528225573,
+                            "y": -0.3120135173281886,
+                            "z": -0.0040737141462420905
+                        }
+                    }
+                }
+            },
+            {
                 "name": "CUBE",
-                "download_url": process.env.PUBLIC_URL + "http://localhost:3000/models/model.stl",
+                "download_url": process.env.PUBLIC_URL + "http://localhost:3000/models/cube.stl",
                 "extension": "stl",
                 "metadata": {
                     "attachPoints": {
@@ -75,30 +167,57 @@ const lib = {
                         },
                     }
                 }
-            },  
+            },
         ],
         "HandR": [
             {
                 "name": "CUBE",
-                "download_url": process.env.PUBLIC_URL + "http://localhost:3000/models/model.stl",
+                "download_url": process.env.PUBLIC_URL + "http://localhost:3000/models/cube.stl",
                 "extension": "stl",
                 "metadata": {
+                    "scale": .4
                 }
             },  
         ],
         "HandL": [
             {
                 "name": "CUBE",
-                "download_url": process.env.PUBLIC_URL + "http://localhost:3000/models/model.stl",
+                "download_url": process.env.PUBLIC_URL + "http://localhost:3000/models/cube.stl",
                 "extension": "stl",
                 "metadata": {
+                    "scale": .4
                 }
             },  
         ],
         "LegR": [
             {
+                "name": "Cylinder",
+                "download_url": process.env.PUBLIC_URL + "http://localhost:3000/models/cylinder.stl",
+                "extension": "stl",
+                "metadata": {
+                    "position": {
+                        "x": 0.0011903476352935934,
+                        "y": -0.4738051962852478,
+                        "z": -0.015501220110232248,
+                    },
+                    "rotation": {
+                        "x": 0,
+                        "y": 0,
+                        "z": -0.5235987755982988,
+                    },
+                    "scale": 0.764753787276196,
+                    "attachPoints": {
+                        "LegR_Foot_R": {
+                            "x": -0.43,
+                            "y": -0.74,
+                            "z": 0.004
+                        },
+                    }
+                }
+            },
+            {
                 "name": "CUBE",
-                "download_url": process.env.PUBLIC_URL + "http://localhost:3000/models/model.stl",
+                "download_url": process.env.PUBLIC_URL + "http://localhost:3000/models/cube.stl",
                 "extension": "stl",
                 "metadata": {
                     "attachPoints": {
@@ -109,12 +228,37 @@ const lib = {
                         },
                     }
                 }
-            },  
+            },
         ],
         "LegL": [
             {
+                "name": "Cylinder",
+                "download_url": process.env.PUBLIC_URL + "http://localhost:3000/models/cylinder.stl",
+                "extension": "stl",
+                "metadata": {
+                    "position": {
+                        "x": 0.0011903476352935934,
+                        "y": -0.4738051962852478,
+                        "z": -0.015501220110232248,
+                    },
+                    "rotation": {
+                        "x": 0,
+                        "y": 0,
+                        "z": 0.5235987755982988,
+                    },
+                    "scale": 0.764753787276196,
+                    "attachPoints": {
+                        "LegL_Foot_L": {
+                            "x": 0.43,
+                            "y": -0.74,
+                            "z": 0.004
+                        },
+                    }
+                }
+            },
+            {
                 "name": "CUBE",
-                "download_url": process.env.PUBLIC_URL + "http://localhost:3000/models/model.stl",
+                "download_url": process.env.PUBLIC_URL + "http://localhost:3000/models/cube.stl",
                 "extension": "stl",
                 "metadata": {
                     "attachPoints": {
@@ -125,23 +269,25 @@ const lib = {
                         },
                     }
                 }
-            },  
+            },
         ],
         "FootR": [
             {
                 "name": "CUBE",
-                "download_url": process.env.PUBLIC_URL + "http://localhost:3000/models/model.stl",
+                "download_url": process.env.PUBLIC_URL + "http://localhost:3000/models/cube.stl",
                 "extension": "stl",
                 "metadata": {
+                    "scale": .4
                 }
             },  
         ],
         "FootL": [
             {
                 "name": "CUBE",
-                "download_url": process.env.PUBLIC_URL + "http://localhost:3000/models/model.stl",
+                "download_url": process.env.PUBLIC_URL + "http://localhost:3000/models/cube.stl",
                 "extension": "stl",
                 "metadata": {
+                    "scale": .4
                 }
             },  
         ]
