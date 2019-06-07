db2008f6031ce091ed59d1c4e99e7d9653cd21d6
Adrian Delgado <adrian.delgadovlaic@gmail.com>
2019-06-07 17:57:59 +0100

    refactor(app): remove unused import

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import { Group } from 'three'
-import axios from 'axios'
 
 import ThreeContainer from '../ThreeContainer'
 import UploadWizard from '../UploadWizard'
