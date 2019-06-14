f8d65d080605a1365bbb129d228469dbc5171511
Adrian Delgado <adrian.delgadovlaic@gmail.com>
2019-06-14 13:12:48 +0100

    fix(wizard): fix camera aspect ratio

--- src/components/UploadWizard/AdjustAttachPoints/three/threeUtils.js
+++ src/components/UploadWizard/AdjustAttachPoints/three/threeUtils.js
@@ -150,6 +150,9 @@ export default {
         // reset renderer size
         const { width, height } = canvas.getBoundingClientRect()
 
+        camera.aspect = width / height
+        camera.updateProjectionMatrix()
+
         renderer.setSize( width, height, false )
         renderer.setPixelRatio( width / height )
     },
