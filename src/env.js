80a053348fa6f48d25b7ed7f3bb9fc78bb94e206
Adrian Delgado <adrian.delgadovlaic@gmail.com>
2020-02-11 14:02:29 +0000

    remove unused files

--- src/env.js
+++
@@ -1,5 +0,0 @@
-export const MMF_HOST = 'https://www.myminifactory.com'
-
-export const API_ENDPOINT = process.env.NODE_ENV === 'production'
-    ? `${MMF_HOST}/api/v2`
-    : 'http://mmf.local/dev.php/api/v2'
