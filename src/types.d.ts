d2a6e5147579d9fde2f9b9183d71f4463f779cf2
Adrian Delgado <adrian.delgadovlaic@gmail.com>
2020-02-20 12:19:41 +0000

    add app props types

--- src/types.d.ts
+++ src/types.d.ts
@@ -5,13 +5,23 @@ export type PartTypeParent = {
 };
 
 export type PartType = {
-    id: number,
-    name: string,
-    label: string,
+    id: number
+    name: string
+    label: string
     parent?: PartTypeParent
 }
 
 export type WorldData = {
+    name: string
+    price: number
+    description: string
+    is_private: boolean
+    image_url: string
+    container_rotation?: Coord3d
+    
+    user_name: string
+    user_url: string
+
     groups: {
         categories: PartType[]
     }[]
@@ -20,6 +30,51 @@ export type WorldData = {
 type Dict<T> = {[key: string]: T}
 
 export type ApiRoutes = {
-    routes: Dict<string>,
+    routes: Dict<string>
     route_params: Dict<string>
 };
+
+export type Coord3d = {
+    x: number
+    y: number
+    z: number
+}
+
+type CustomizedMesh = {
+    id: number
+    [key: string]: any // TODO
+};
+
+export type CustomizerPart = {
+    id: number
+    name: string
+    img: string
+    files: {
+        default: {
+            extension: string
+            url: string
+        }
+    }
+    metadata?: {
+        position?: Coord3d
+        rotation?: Coord3d
+        scale?: number
+        attachPoints?: {[attachPointName: string]: Coord3d}
+    }
+}
+
+export type Objects_from_props = {
+    allPartTypeIds: number[],
+    byPartTypeId: {[partTypeId: string]: CustomizerPart[]}
+}
+
+export type AppProps = {
+    api: ApiRoutes
+    customizedMeshes: CustomizedMesh[]
+    customizedMeshesInCart: number[] // ids
+    customizedMeshesOwnedByUser: number[] // ids
+    customizer_pay_per_download_enabled: boolean
+    edit_mode: boolean
+    objects: Objects_from_props
+    worldData: WorldData
+}
