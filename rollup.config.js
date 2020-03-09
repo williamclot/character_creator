68b93c3d11eac931583ed893c695ecbbd8067922
Adrian Delgado <adrian.delgadovlaic@gmail.com>
2020-03-09 16:32:46 +0000

    fix eslint problems in rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,44 +1,42 @@
-import babel from 'rollup-plugin-babel'
+import babel from 'rollup-plugin-babel';
 import resolve from '@rollup/plugin-node-resolve';
 import images from '@rollup/plugin-image';
-import autoExternal from 'rollup-plugin-auto-external'
-import postcss from 'rollup-plugin-postcss'
-
+import autoExternal from 'rollup-plugin-auto-external';
+import postcss from 'rollup-plugin-postcss';
 
 import pkg from './package.json';
 
 const extensionsToProcess = ['.js', '.ts'];
 
 export default {
-	input: 'src/index.js',
-	
-	plugins: [
-		autoExternal(),
-		resolve({
-			extensions: extensionsToProcess
-		}),
+    input: 'src/index.js',
 
-		postcss({
-			extract: true,
-			// modules: true,
-			autoModules: true
-		}),
+    plugins: [
+        autoExternal(),
+        resolve({
+            extensions: extensionsToProcess,
+        }),
 
-		images(),
-		babel({
-			extensions: extensionsToProcess,
-			exclude: 'node_modules/**'
-		}),
-	],
-	output: [
-		/*
+        postcss({
+            extract: true,
+            // modules: true,
+            autoModules: true,
+        }),
+
+        images(),
+        babel({
+            extensions: extensionsToProcess,
+            exclude: 'node_modules/**',
+        }),
+    ],
+    output: [
+        /*
 			Currently, the target environment knows how to handle es,
 			so until another target is needed, es will be the only output
 		*/
-		{
-			file: pkg.module,
-			format: 'es'
-		},
-
-	]
+        {
+            file: pkg.module,
+            format: 'es',
+        },
+    ],
 };
